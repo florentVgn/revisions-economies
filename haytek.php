f7c976cd485e80c1ebbcffd2ae1d7c3f626ec68a
Florent Viogne <viognef@gmail.com>
2016-12-18 17:13:58 +0100

    menu php

--- haytek.php
+++ haytek.php
@@ -43,34 +43,9 @@
                     <!-- End Logo -->
                 </div>
                 <!-- Top Menu -->
-                <div id="hornav" class="row text-light">
-                    <div class="col-md-12">
-                        <div class="text-center visible-lg">
-                            <ul id="hornavmenu" class="nav navbar-nav">
-                                <li>
-                                    <a href="index.html" class="fa-home active">Accueil</a>
-                                </li>
-								<li>
-                                                                        <a href="menuAuteurs.html" class="fa-briefcase">Les Auteurs</a>
-                                </li>
-								<li>
-                                    <span class="fa-file-text-o">La dissertation</span>
-                                    <ul>
-                                        <li>
-                                            <a href="methodologie.html">La méthodologie</a>
-                                        </li>
-                                        <li>
-                                            <a href="sujet_1.html">Un exemple de dissertation</a>
-                                        </li>
-                                    </ul>
-                                </li>
-								<li>
-                                    <a href="théorie.html" class="fa-money">Notre théorie</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                include 'menu.php';
+                ?>
                 <!-- End Top Menu -->
             </div>
         </div>
