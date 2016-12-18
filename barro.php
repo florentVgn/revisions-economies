b95f0c08826c3790abcef3859efe47a2159a8121
Florent Viogne <viognef@gmail.com>
2016-12-18 16:39:23 +0100

    test menu en php

--- barro.php
+++ barro.php
@@ -49,7 +49,7 @@
                             <ul id="hornavmenu" class="nav navbar-nav">
                                 <li>
                                     <a href="index.html" class="fa-home active">Accueil</a>
-                                </li> 
+                                </li>
 								<li>
                                                                         <a href="menuAuteurs.html" class="fa-briefcase">Les Auteurs</a>
                                 </li>
@@ -84,7 +84,7 @@
                         <h2 class="margin-bottom-20">ROBERT JOSEPH BARRO (1944–)</h2>
                         <!-- Intro / Why Us? -->
                         <div class="row">
-					
+
 							<div class="col-md-8 animate fadeInLeft">
 								<h3>PROFESSION:</h3>
 									<p>Macroéconomiste américain de réputation mondiale, professeur d'économie à l'université Harvard et Fellow à la Hoover Institution à l'université Stanford.</p>
@@ -96,15 +96,15 @@
                             </div>
                         </div>
                         <!-- End Intro / Why Us? -->
-                        
+
                         <!-- Top Panels -->
-                       
+
                         <hr class="margin-top-3	0">
                         <!-- Middle Text -->
                         <div class="row">
                             <div class="col-md-12 text-center margin-top-10 animate fadeInUp">
                                 <h2 class="text-center margin-top-10">“C'est accorder un trop grand crédit à la nature humaine que de laisser à l'individu le soin de prendre en considération l'intérêt de la communauté.”</h2>
-                                
+
                             </div>
                         </div>
                         <!-- End Middle Text -->
@@ -122,26 +122,26 @@
                     <div class="col-md-8 margin-bottom-30">
                         <h3 class="margin-bottom-10">Description du mouvement:</h3>
                         <p>L'école néoclassique est un terme générique utilisé pour désigner plusieurs courants économiques qui étudient la formation des prix, de la production et de la distribution des revenus à travers le mécanisme d'offre et de demande sur un marché.</p>
-                            
+
                     </div>
                     <!-- End Disclaimer -->
-               
+
                     <!-- Friends -->
                     <div class="col-sm-6 col-md-3 margin-bottom-30">
                         <h3 class="margin-bottom-10">Auteurs néo-classiques</h3>
                         <ul class="menu">
 								<li>
 									<a href="buchanan.html">James McGill Buchanan Jr.</a>
-								</li>									
+								</li>
 								<li>
 									<a href="tullock.html">Gordon Tullock</a>
-								</li>	
+								</li>
 								<li>
 									<a href="downs.html">Anthony Downs</a>
-								</li>								
+								</li>
 								<li>
 									<a href="haytek.html">Friedrich Hayek</a>
-								</li>						
+								</li>
                         </ul>
                         <div class="clearfix"></div>
                     </div>
@@ -149,7 +149,7 @@
                 </div>
             </div>
         </div>
-       
+
             <!-- JS -->
             <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
             <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
